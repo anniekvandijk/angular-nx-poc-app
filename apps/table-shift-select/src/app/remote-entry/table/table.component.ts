028fe850295f443f938db1409c065751f257770c
Anniek van Dijk <anniek.vandijk@getthere.nl>
2023-07-17 13:14:35 +0200

    select

--- apps/table-shift-select/src/app/remote-entry/table/table.component.ts
+++ apps/table-shift-select/src/app/remote-entry/table/table.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTableModule, MatTable } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSortModule, MatSort } from '@angular/material/sort';
 import { TableDataSource, TableItem } from './table-datasource';
 import { CommonModule, NgStyle } from '@angular/common';
@@ -10,7 +11,7 @@ import { CommonModule, NgStyle } from '@angular/common';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule, NgStyle ]
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule, MatCheckboxModule, NgStyle ]
 })
 export class TableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -18,9 +19,9 @@ export class TableComponent implements AfterViewInit {
   @ViewChild(MatTable) table!: MatTable<TableItem>;
   dataSource: TableDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
-  selectedRows: TableItem[] = [];
+  displayedColumns = ['select', 'id', 'name'];
+  selectedItems = new Set<TableItem>();
+  lastSelectedRowIndex: number | null = null;
 
   constructor() {
     this.dataSource = new TableDataSource();
@@ -30,5 +31,42 @@ export class TableComponent implements AfterViewInit {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
+  }
+
+  isItemSelected(row: TableItem): boolean {
+    return this.selectedItems.has(row);
+  }
+
+  toggleItemSelection(row: TableItem): void {
+    if (this.isItemSelected(row)) {
+      this.selectedItems.delete(row);
+    } else {
+      this.selectedItems.add(row);
+    }
+  }
+
+  masterToggle() {
+    if (this.selectedItems.size === this.dataSource.data.length && this.dataSource.data.length > 0) {
+      this.selectedItems.clear();
+    } else {
+      this.dataSource.data.forEach(row => this.selectedItems.add(row));
+    }
+  }
+
+  onMouseDown(event: MouseEvent, row: TableItem) {
+    event.stopPropagation();
+
+
+      const rowIndex = this.dataSource.data.indexOf(row);
+      if (event.shiftKey && this.lastSelectedRowIndex !== null) {
+        const start = Math.min(this.lastSelectedRowIndex, rowIndex);
+        const end = Math.max(this.lastSelectedRowIndex, rowIndex);
+        for (let i = start; i < end; i++) {
+          this.selectedItems.add(this.dataSource.data[i]);
+        }
+      
+    } else {
+      this.lastSelectedRowIndex = this.dataSource.data.indexOf(row);
+    }
   }
 }
